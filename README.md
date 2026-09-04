7f51353300eccd20b96043354adc7b08a2876d58
Kavin Mohan <kavinmk.official@gmail.com>
2026-09-04 21:56:49 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# DevOps_CI_<Your_Roll_Number>
+
+Simple Python app for DevOps Assignment 2 (Git + Jenkins CI).
+
+## Run
+```bash
+python src/app.py
---
+++ src/app.py
@@ -0,0 +1,9 @@
+def greet(name="World"):
+    return f"Hello, {name}!"
+
+def add(a, b):
+    return a + b
+
+if __name__ == "__main__":
+    print(greet())
+    print(add(2, 3))
